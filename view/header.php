231337d9e90aecea81b466c9d062533e8e085546
efrei-tc <titouan.charlet@efrei.net>
2025-01-15 16:56:02 +0100

    page tous les produits

--- view/header.php
+++ view/header.php
@@ -128,7 +128,89 @@
   </nav>
 
 
-  
+  <div id="features-products-all">
+<div id="features-products-top-all">
+<h1>Explorez toutes<br>nos montres de luxe</h1>
+<p>Explorez des modèles issus des plus grandes maisons horlogères et laissez-vous séduire par l'élégance intemporelle et la précision inégalée de chaque montre.</p>
+</div>
+
+<div id="product-filter">
+    <select name="category" id="category">
+      <option value="all">Toutes les montres</option>
+      <option value="quartz">Quartz</option>
+      <option value="mecanique">Mécanique</option>
+    </select>
+  </div>
+
+<div id="features-products-container">
+
+<a href="?page=produits" id="features-products-container-item">
+  <div id="features-products-container-item-img">
+    <img src="../img/features-products-bottom1.webp" alt="features-products-bottom">
+  </div>
+  <div id="features-products-container-item-text">
+    <div id="features-products-container-item-text-top">
+      <p class="medium-weight-text">Montre 1</p>
+      <p class="small-text">Quartz</p>
+    </div>
+    <div id="features-products-container-item-text-bottom">
+      <p>15 000€</p>
+    </div>
+  </div>
+</a>
+
+<a href="?page=produits" id="features-products-container-item">
+  <div id="features-products-container-item-img">
+    <img src="../img/features-products-bottom2.webp" alt="features-products-bottom">
+  </div>
+  <div id="features-products-container-item-text">
+    <div id="features-products-container-item-text-top">
+      <p class="medium-weight-text">Montre 2</p>
+      <p class="small-text">Mécanique</p>
+    </div>
+    <div id="features-products-container-item-text-bottom">
+      <p>25 000€</p>
+    </div>
+  </div>
+</a>
+
+<a href="?page=produits" id="features-products-container-item">
+  <div id="features-products-container-item-img">
+    <img src="../img/features-products-bottom3.webp" alt="features-products-bottom">
+  </div>
+  <div id="features-products-container-item-text">
+    <div id="features-products-container-item-text-top">
+      <p class="medium-weight-text">Montre 3</p>
+      <p class="small-text">Quartz</p>
+    </div>
+    <div id="features-products-container-item-text-bottom">
+      <p>36 000€</p>
+    </div>
+  </div>
+</a>
+
+<a href="?page=produits" id="features-products-container-item">
+  <div id="features-products-container-item-img">
+    <img src="../img/features-products-bottom4.webp" alt="features-products-bottom">
+  </div>
+  <div id="features-products-container-item-text">
+    <div id="features-products-container-item-text-top">
+      <p class="medium-weight-text">Montre 4</p>
+      <p class="small-text">Mécanique</p>
+    </div>
+    <div id="features-products-container-item-text-bottom">
+      <p>6 000€</p>
+    </div>
+  </div>
+</a>
+
+
+</div>
+
+</div>
+
+</div>
+
 
 
 <footer>
