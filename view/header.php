c6c8847cb2d51c49224ca63f9cd56acc7eb01c0f
efrei-tc <titouan.charlet@efrei.net>
2025-01-16 09:59:19 +0100

    page mes produits

--- view/header.php
+++ view/header.php
@@ -128,76 +128,7 @@
   </nav>
 
 
-<div id="features-products">
-  <div id="features-products-top">
-    <h2>Vos montres en vente</h2>
-  </div>
-  <div id="features-products-container">
 
-  <a href="?page=produits" id="features-products-container-item">
-    <div id="features-products-container-item-img">
-      <img src="../img/features-products-bottom1.webp" alt="features-products-bottom">
-    </div>
-    <div id="features-products-container-item-text">
-      <div id="features-products-container-item-text-top">
-        <p class="medium-weight-text">Montre 1</p>
-        <p class="small-text">Quartz</p>
-      </div>
-      <div id="features-products-container-item-text-bottom">
-        <p>15 000€</p>
-      </div>
-    </div>
-  </a>
-
-  <a href="?page=produits" id="features-products-container-item">
-    <div id="features-products-container-item-img">
-      <img src="../img/features-products-bottom2.webp" alt="features-products-bottom">
-    </div>
-    <div id="features-products-container-item-text">
-      <div id="features-products-container-item-text-top">
-        <p class="medium-weight-text">Montre 2</p>
-        <p class="small-text">Mécanique</p>
-      </div>
-      <div id="features-products-container-item-text-bottom">
-        <p>25 000€</p>
-      </div>
-    </div>
-  </a>
-
-  <a href="?page=produits" id="features-products-container-item">
-    <div id="features-products-container-item-img">
-      <img src="../img/features-products-bottom3.webp" alt="features-products-bottom">
-    </div>
-    <div id="features-products-container-item-text">
-      <div id="features-products-container-item-text-top">
-        <p class="medium-weight-text">Montre 3</p>
-        <p class="small-text">Quartz</p>
-      </div>
-      <div id="features-products-container-item-text-bottom">
-        <p>36 000€</p>
-      </div>
-    </div>
-  </a>
-
-  <a href="?page=produits" id="features-products-container-item">
-    <div id="features-products-container-item-img">
-      <img src="../img/features-products-bottom4.webp" alt="features-products-bottom">
-    </div>
-    <div id="features-products-container-item-text">
-      <div id="features-products-container-item-text-top">
-        <p class="medium-weight-text">Montre 4</p>
-        <p class="small-text">Mécanique</p>
-      </div>
-      <div id="features-products-container-item-text-bottom">
-        <p>6 000€</p>
-      </div>
-    </div>
-  </a>
-
-
-  </div>
-
-</div>
 
 
 
