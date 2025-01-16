09071cc1bb17d19dcfc34d9ad9ffe4cd80ad3b49
efrei-tc <titouan.charlet@efrei.net>
2025-01-16 12:59:02 +0100

    page connexion

--- view/header.php
+++ view/header.php
@@ -45,3 +45,21 @@
   </nav>
 
 
+  <div id="formulaire">
+  <div id="formulaire-top">
+    <h1>Connexion</h1>
+    <p>Remplissez le formulaire ci-dessous pour vous connecter.</p>
+  </div>
+
+  <form id="formulaire-bottom" action="?page=connexion" method="post">
+    <div id="formulaire-bottom-top">
+      <input type="text" name="email" placeholder="Email">
+      <input type="text" name="mot_de_passe" placeholder="Mot de passe">
+    </div>
+    <div id="formulaire-button-container-vertical">
+      <input class="btn-primary" type="submit" value="Se connecter">
+      <a href="?page=inscription">Vous n'avez pas de compte ? Inscrivez-vous</a>
+    </div>
+  </form>
+
+</div>
