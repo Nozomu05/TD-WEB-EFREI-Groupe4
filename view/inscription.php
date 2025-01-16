8a9c3b2e99bbb60cd040c0914be1b77d6fdf565a
efrei-tc <titouan.charlet@efrei.net>
2025-01-16 12:46:30 +0100

    pages ajoutées

--- view/inscription.php
+++ view/inscription.php
@@ -1,4 +1,4 @@
-<h1>Inscription</h1>
+<!--<h1>Inscription</h1>
 <form action="" method="post">
   <fieldset>
     <div>
@@ -25,4 +25,29 @@
     </div>
     <button type="submit" class="btn btn-primary">Inscription</button>
   </fieldset>
-</form>
+</form>-->
+
+<div id="formulaire">
+  <div id="formulaire-top">
+    <h1>Inscription</h1>
+    <p>Remplissez le formulaire ci-dessous pour créer votre compte.</p>
+  </div>
+
+  <form id="formulaire-bottom" action="?page=finalisation" method="post">
+    <div id="formulaire-bottom-top">
+      <div id="nom-container">
+        <input type="text" name="prenom" placeholder="Prénom">
+        <input type="text" name="nom" placeholder="Nom">
+      </div>
+      <input type="date" name="date_naissance" placeholder="Date de naissance">
+      <input type="text" name="pseudo" placeholder="Pseudo">
+      <input type="text" name="email" placeholder="Email">
+      <input type="text" name="mot_de_passe" placeholder="Mot de passe">
+    </div>
+    <div id="formulaire-button-container-vertical">
+      <input class="btn-primary" type="submit" value="S'inscrire maintenant">
+      <a href="?page=connexion">Vous avez déjà un compte ? Connectez-vous</a>
+    </div>
+  </form>
+
+</div>
--- view/utilisateur.php
+++ view/utilisateur.php
@@ -1,6 +1,6 @@
 <h1>Mes Infos</h1>
 <?php
-    foreach($infos as $cle => $info) {
+   /* foreach($infos as $cle => $info) {
         if($cle !== "id_user"){
             if($cle === "mdp"){
                 ?>
@@ -14,5 +14,28 @@
                 <?php
             }
         }
-    }
+    }*/
 ?>
+
+<div id="formulaire">
+  <div id="formulaire-top">
+    <h1>Mon profil</h1>
+  </div>
+
+  <form id="formulaire-bottom" action="?page=finalisation" method="post">
+    <div id="formulaire-bottom-top">
+      <div id="nom-container">
+        <input type="text" name="prenom" placeholder="Prénom">
+        <input type="text" name="nom" placeholder="Nom">
+      </div>
+      <input type="text" name="email" placeholder="Email">
+      <input type="text" name="mot_de_passe" placeholder="Nouveau mot de passe">
+      <input type="text" name="mot_de_passe_confirmation" placeholder="Confirmer mot de passe">
+    </div>
+    <div id="formulaire-button-container">
+      <input class="btn-primary" type="submit" value="Sauvegarder">
+      <input class="btn-delete" type="submit" value="Supprimer mon compte">
+    </div>
+  </form>
+
+</div>
