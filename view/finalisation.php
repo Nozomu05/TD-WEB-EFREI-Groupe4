67888e983b2b5b14367903f431dbcda168669b58
efrei-tc <titouan.charlet@efrei.net>
2025-01-16 11:13:31 +0100

    page panierV2

--- view/finalisation.php
+++ view/finalisation.php
@@ -1,8 +1,8 @@
 
 
-<h1>mon panier<h1>
+<!--<h1>mon panier<h1>
     
-<?php foreach($produits as $produit){
+<?php/* foreach($produits as $produit){
     ?> 
         <a href="?page=produit&id=<?=$produit['id_produit']?>">
             <button>
@@ -23,6 +23,87 @@ if($pasvide){
             <button>Payer</button>
         </a>
     <?php
-}
+}*/
 ?>
 
+-->
+
+
+<div id="features-products">
+  <div id="basket-products-top">
+      <div id="basket-products-top-left">
+        <h1 class="element-left">Votre panier</h1>
+        <p class="element-left">Félicitations ! Vous avez choisi de magnifiques montres.</p>
+      </div>
+      <a class="btn-primary" href="?page=produits">Tout supprimer</a>
+  </div>
+  <div id="features-products-container">
+
+  <a href="?page=produits" id="features-products-container-item">
+    <div id="features-products-container-item-img">
+      <img src="../img/features-products-bottom1.webp" alt="features-products-bottom">
+    </div>
+    <div id="features-products-container-item-text">
+      <div id="features-products-container-item-text-top">
+        <p class="medium-weight-text">Montre 1</p>
+        <p class="small-text">Quartz</p>
+      </div>
+      <div id="features-products-container-item-text-bottom">
+        <p>15 000€</p>
+      </div>
+    </div>
+    <input class="btn-primary" type="submit" value="Supprimer">
+  </a>
+
+  <a href="?page=produits" id="features-products-container-item">
+    <div id="features-products-container-item-img">
+      <img src="../img/features-products-bottom2.webp" alt="features-products-bottom">
+    </div>
+    <div id="features-products-container-item-text">
+      <div id="features-products-container-item-text-top">
+        <p class="medium-weight-text">Montre 2</p>
+        <p class="small-text">Mécanique</p>
+      </div>
+      <div id="features-products-container-item-text-bottom">
+        <p>25 000€</p>
+      </div>
+    </div>
+    <input class="btn-primary" type="submit" value="Supprimer">
+  </a>
+
+  <a href="?page=produits" id="features-products-container-item">
+    <div id="features-products-container-item-img">
+      <img src="../img/features-products-bottom3.webp" alt="features-products-bottom">
+    </div>
+    <div id="features-products-container-item-text">
+      <div id="features-products-container-item-text-top">
+        <p class="medium-weight-text">Montre 3</p>
+        <p class="small-text">Quartz</p>
+      </div>
+      <div id="features-products-container-item-text-bottom">
+        <p>36 000€</p>
+      </div>
+    </div>
+    <input class="btn-primary" type="submit" value="Supprimer">
+  </a>
+
+  <a href="?page=produits" id="features-products-container-item">
+    <div id="features-products-container-item-img">
+      <img src="../img/features-products-bottom4.webp" alt="features-products-bottom">
+    </div>
+    <div id="features-products-container-item-text">
+      <div id="features-products-container-item-text-top">
+        <p class="medium-weight-text">Montre 4</p>
+        <p class="small-text">Mécanique</p>
+      </div>
+      <div id="features-products-container-item-text-bottom">
+        <p>6 000€</p>
+      </div>
+    </div>
+    <input class="btn-primary" type="submit" value="Supprimer">
+  </a>
+
+
+  </div>
+
+</div>
