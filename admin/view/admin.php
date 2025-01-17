cdc750a3ea83b3d815d8a9a57d3bf54cd06953be
efrei-tc <titouan.charlet@efrei.net>
2025-01-17 11:43:35 +0100

    ajout alpine confirmationV2

--- admin/view/admin.php
+++ admin/view/admin.php
@@ -37,18 +37,19 @@
     </table>
 
     <!-- Boîte de dialogue de confirmation -->
-    <div x-show="showConfirmDialog" 
-         style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
-        <div style="background: white; padding: 2rem; border-radius: 0.5rem; text-align: center;">
-            <p>Êtes-vous sûr de vouloir supprimer cet utilisateur ?</p>
-            <div style="margin-top: 1rem;">
-                <button @click="window.location.href='?page=deleteTask&id_task=' + userToDelete" class="btn-delete">
-                    Confirmer
-                </button>
-                <button @click="showConfirmDialog = false" style="margin-left: 1rem;">
-                    Annuler
-                </button>
+    <template x-if="showConfirmDialog">
+        <div style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0,0,0,0.5); display: flex; justify-content: center; align-items: center;">
+            <div style="background: white; padding: 2rem; border-radius: 0.5rem; text-align: center;">
+                <p>Êtes-vous sûr de vouloir supprimer cet utilisateur ?</p>
+                <div style="margin-top: 1rem;">
+                    <button @click="window.location.href='?page=deleteTask&id_task=' + userToDelete" class="btn-delete">
+                        Confirmer
+                    </button>
+                    <button @click="showConfirmDialog = false" style="margin-left: 1rem;">
+                        Annuler
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
+    </template>
 </div>
